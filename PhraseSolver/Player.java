2ace5f4f918cf25d7a9b3e78de06f176afb9207f
Hariswaaaar <hbaburaj56@ladueschools.net>
2022-11-09 18:56:55 +0000

    fhdsafjhkds

--- PhraseSolver/Player.java
+++ PhraseSolver/Player.java
@@ -1,5 +1,27 @@
 package PhraseSolver;
 
 public class Player {
-    
+    private String name;
+    private int score;
+
+    public Player(String Name, int dollars){
+        name = Name;
+        score = dollars;
+    }
+
+    public void setScore(int scores){
+        score = scores;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
 }
